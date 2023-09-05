f442f7b45d7118cf12cc9450e91f91def5be91d4
Cyndowow <71725217+Cyndowow@users.noreply.github.com>
2023-09-05 10:47:03 +0200

    Add post handling

--- routes/index.js
+++ routes/index.js
@@ -19,4 +19,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Mini-Messageboard', messages: messages });
 });
 
+router.post("/new", function(req, res, next) {
+  const userName = req.body.messageUser;
+  const message = req.body.messageText;
+  messages.push({text: message, user: userName, added: new Date() });
+  res.redirect("/")
+})
+
 module.exports = router;
